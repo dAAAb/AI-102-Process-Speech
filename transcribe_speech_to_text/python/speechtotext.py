85d5a98061a4d2cd20f3887b19abfa18cc6ea198
dAb 大寶 <dAb_Ko@hotmail.com>
2020-06-06 10:50:39 +0000

    checkout

--- transcribe_speech_to_text/python/speechtotext.py
+++ transcribe_speech_to_text/python/speechtotext.py
@@ -1,4 +1,5 @@
 import azure.cognitiveservices.speech as speechsdk
+import time
 
 # Creates an instance of a speech config with specified subscription key and service region.
 # Replace with your own subscription key and service region (e.g., "westus").
@@ -11,7 +12,26 @@ audio_filename = "transcribe_speech_to_text/media/1min.wav"
 audio_input = speechsdk.AudioConfig(filename=audio_filename)
 
 # Creates a recognizer with the given settings
-speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config, audio_config=audio_input)
+
+speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config, language="zh-TW", audio_config=audio_input)
+
+done = False
+
+def stop_cb(evt):
+    """callback that signals to stop continuous recognition upon receiving an event `evt`"""
+    print('CLOSING on {}'.format(evt))
+    # nonlocal done
+    done = True
+
+# Connect callbacks to the events fired by the speech recognizer
+speech_recognizer.recognizing.connect(lambda evt: print('RECOGNIZING: {}'.format(evt)))
+speech_recognizer.recognized.connect(lambda evt: print('RECOGNIZED: {}'.format(evt)))
+speech_recognizer.session_started.connect(lambda evt: print('SESSION STARTED: {}'.format(evt)))
+speech_recognizer.session_stopped.connect(lambda evt: print('SESSION STOPPED {}'.format(evt)))
+speech_recognizer.canceled.connect(lambda evt: print('CANCELED {}'.format(evt)))
+# stop continuous recognition on either session stopped or canceled events
+speech_recognizer.session_stopped.connect(stop_cb)
+speech_recognizer.canceled.connect(stop_cb)
 
 print("Recognizing first result...")
 
@@ -21,8 +41,13 @@ print("Recognizing first result...")
 # Note: Since recognize_once() returns only a single utterance, it is suitable only for single
 # shot recognition like command or query. 
 # For long-running multi-utterance recognition, use start_continuous_recognition() instead.
-result = speech_recognizer.recognize_once()
+# result = speech_recognizer.recognize_once()
 
+result = speech_recognizer.start_continuous_recognition()
+while not done:
+    time.sleep(.5)
+
+speech_recognizer.stop_continuous_recognition()
 # Checks result.
 if result.reason == speechsdk.ResultReason.RecognizedSpeech:
     print("Recognized: {}".format(result.text))
